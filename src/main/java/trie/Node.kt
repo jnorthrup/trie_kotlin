253b10b6b1ed0bfba90c8d08fa217650c57541b9
Jim Northrup <jim@vsiwest.com>
2020-07-03 23:43:27 +0700

    associative sets

--- src/main/java/trie/Node.kt
+++ src/main/java/trie/Node.kt
@@ -3,6 +3,8 @@ package trie
 /**
  * Created by kenny on 6/6/16.
  */
-data class Node<T>(val value: T?,
-                var isLeaf: Boolean,
-                val children: MutableMap<T, Node<T>> = mutableMapOf())
+data class Node<T, V>(val value: T?,
+                      var isLeaf: Boolean,
+                      val payload: V,
+                      val children: MutableMap<T,Node<T,V>> = mutableMapOf()
+)
--- src/test/java/trie/TrieTest.kt
+++ src/test/java/trie/TrieTest.kt
@@ -1,6 +1,8 @@
 package trie;
 
 import org.junit.Test;
+import vec.util.*
+
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -8,55 +10,56 @@ import kotlin.test.assertTrue
  * Created by kenny on 6/6/16.
  */
 class TrieTest {
+    val d = Double.POSITIVE_INFINITY
 
     @Test
     fun stringTrie() {
-        val trie = Trie<String>()
-        assertFalse(trie.contains(emptyList()))
+        val trie = Trie<String, Any?>()
+        assertFalse(trie.contains(*emptyArray()))
 
-        trie.add(arrayListOf("a", "b", "c"))
-        assertFalse(trie.contains(arrayListOf("a")))
-        assertFalse(trie.contains(arrayListOf("a", "b")))
-        assertTrue(trie.contains(arrayListOf("a", "b", "c")))
+        trie.add(d, "a", "b", "c")
+        assertFalse(trie.contains( "a" ))
+        assertFalse(trie.contains( "a", "b" ))
+        assertTrue(trie.contains( "a", "b", "c" ))
 
-        trie.add(arrayListOf("a", "b"))
-        assertTrue(trie.contains(arrayListOf("a", "b")))
+        trie.add(d,"a", "b")
+        assertTrue(trie.contains( "a", "b" ))
 
-        trie.add(arrayListOf("a", "b", "d", "e"))
-        assertTrue(trie.contains(arrayListOf("a", "b", "d", "e")))
+        trie.add( d,"a", "b", "d", "e")
+        assertTrue(trie.contains( "a", "b", "d", "e" ))
     }
 
     @Test
     fun charTrie() {
-        val trie = Trie<Char>()
-        assertFalse(trie.contains(emptyList()))
+        val trie = Trie<Char, Any?>()
+        assertFalse(trie.contains(*emptyArray()))
 
-        trie.add(arrayListOf('a', 'b', 'c'))
-        assertFalse(trie.contains(arrayListOf('a')))
-        assertFalse(trie.contains(arrayListOf('a', 'b')))
-        assertTrue(trie.contains(arrayListOf('a', 'b', 'c')))
+        trie.add(d, 'a', 'b', 'c')
+        assertFalse(trie.contains('a'))
+        assertFalse(trie.contains('a', 'b'))
+        assertTrue(trie.contains('a', 'b', 'c'))
 
-        trie.add(arrayListOf('a', 'b'))
-        assertTrue(trie.contains(arrayListOf('a', 'b')))
+        trie.add(d, 'a', 'b')
+        assertTrue(trie.contains('a', 'b'))
 
-        trie.add(arrayListOf('a', 'b', 'd', 'e'))
-        assertTrue(trie.contains(arrayListOf('a', 'b', 'd', 'e')))
+        trie.add(d, 'a', 'b', 'd', 'e')
+        assertTrue(trie.contains('a', 'b', 'd', 'e'))
     }
 
     @Test
     fun intTrie() {
-        val trie = Trie<Int>()
-        assertFalse(trie.contains(emptyList()))
+        val trie = Trie<Int, Any?>()
+        assertFalse(trie.contains(*emptyArray()))
 
-        trie.add(arrayListOf(1, 2, 3))
-        assertFalse(trie.contains(arrayListOf(1)))
-        assertFalse(trie.contains(arrayListOf(1, 2)))
-        assertTrue(trie.contains(arrayListOf(1, 2 ,3)))
+        trie.add(d, 1, 2, 3)
+        assertFalse(trie.contains(  1))
+        assertFalse(trie.contains(  1, 2))
+        assertTrue(trie.contains( 1, 2, 3))
 
-        trie.add(arrayListOf(1, 2))
-        assertTrue(trie.contains(arrayListOf(1, 2)))
+        trie.add(d, 1, 2)
+        assertTrue(trie.contains(1,2))
 
-        trie.add(arrayListOf(1, 2, 4, 5))
-        assertTrue(trie.contains(arrayListOf(1, 2, 4, 5)))
+        trie.add(d,  1, 2, 4, 5)
+        assertTrue(trie.contains( 1, 2, 4, 5 ))
     }
 }
